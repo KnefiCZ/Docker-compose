312090132ad996d06e474d993959726c6d9bf61b
KnefiCZ <matej.kneifl@seznam.cz>
2021-01-21 14:58:32 +0100

    jo
    no udelal jsem to nooo mhm

---
+++ .docker/apache2/vhosts/knefisite.cz/index.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kefisite</title>
+</head>
+<body>
+    <?php echo "knefiho webová stránka nepog!"; ?>
+</body>
+</html>
